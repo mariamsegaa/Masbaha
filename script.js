3bc5a6e14a9a85cd17a9651278e3e21805618ad8
Mariam Segaa <119626206+mariamsegaa@users.noreply.github.com>
2025-05-17 22:35:16 +0300

    script.js

--- script.js
+++ script.js
@@ -1,34 +1,34 @@
-let counts = {
-  "سبحان الله": 0,
-  "الحمد لله": 0,
-  "الله أكبر": 0,
-  "لا إله إلا الله": 0
-};
+  let counts = {
+      "اللهم أدخلهم الجنة": 0,
+      " اللهم آنس وحشتهم": 0,
+      " اللهم أنر قبرهم": 0,
+      "اللهم ارض عنهم وتقبلهم من الصالحين": 0
+    };
 
-const countDisplay = document.getElementById("count");
-const tasbeehBtn = document.getElementById("tasbeehBtn");
-const resetBtn = document.getElementById("resetBtn");
-const zikrSelect = document.getElementById("zikrSelect");
+    const countDisplay = document.getElementById("count");
+    const tasbeehBtn = document.getElementById("tasbeehBtn");
+    const resetBtn = document.getElementById("resetBtn");
+    const zikrSelect = document.getElementById("zikrSelect");
 
-let currentZikr = zikrSelect.value;
+    let currentZikr = zikrSelect.value;
 
-window.addEventListener("DOMContentLoaded", () => {
-  countDisplay.textContent = counts[currentZikr];
-});
+    window.addEventListener("DOMContentLoaded", () => {
+      countDisplay.textContent = counts[currentZikr];
+    });
 
-zikrSelect.addEventListener("change", () => {
-  currentZikr = zikrSelect.value;
-  countDisplay.textContent = counts[currentZikr];
-});
+    zikrSelect.addEventListener("change", () => {
+      currentZikr = zikrSelect.value;
+      countDisplay.textContent = counts[currentZikr];
+    });
 
-tasbeehBtn.addEventListener("click", () => {
-  if (counts[currentZikr] < 10) {
-    counts[currentZikr]++;
-    countDisplay.textContent = counts[currentZikr];
-  }
-});
+    tasbeehBtn.addEventListener("click", () => {
+      if (counts[currentZikr] < 10) {
+        counts[currentZikr]++;
+        countDisplay.textContent = counts[currentZikr];
+      }
+    });
 
-resetBtn.addEventListener("click", () => {
-  counts[currentZikr] = 0;
-  countDisplay.textContent = counts[currentZikr];
-});
+    resetBtn.addEventListener("click", () => {
+      counts[currentZikr] = 0;
+      countDisplay.textContent = counts[currentZikr];
+    });
